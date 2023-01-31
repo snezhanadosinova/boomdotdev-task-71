52ca665479573df990ceb0433a8a66058c0c8bed
snezhanadosinova <sdosinova@gmail.com>
2023-01-31 14:12:54 +0200

    fix: added errors

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [terms, setTerms] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch(`https://jaspervdj.be/lorem-markdownum/markdown.txt`)
       .then((restult) => restult.text())
-      .then((terms) => setTerms(terms));
+      .then((terms) => setTerms(terms))
+      .catch((error) => {
+        setError(error.message);
+      });
   }, []);
 
   return (
     <div className="App">
-      <Document title="Terms and Conditions" content={terms} />
+      {(error && <p>An error has occurred: {error}</p>) || (
+        <Document title="Terms and Conditions" content={terms} />
+      )}
     </div>
   );
 }
